c9e740834fa1dba7baeeb36f1d439db50080d59f
AlteredCoder <AlteredCoder>
2021-01-07 16:46:04 +0100

    add debug

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ PLUGIN_NAME="cs-ingress-nginx-bouncer"
 PLUGIN_PATH="/etc/nginx/lua/plugins/${PLUGIN_NAME}"
 
 mkdir -p ${PLUGIN_PATH}
+pwd
 git clone https://github.com/crowdsecurity/lua-cs-bouncer.git ${PLUGIN_PATH}/
 wget https://raw.githubusercontent.com/AlteredCoder/poc-ingress-nginx-bouncer/main/main.lua
+ls /etc/nginx/lua/plugins/
 cp main.lua ${PLUGIN_PATH}/
+echo "OK"
