dd4c51d75e65bdf5fe572daa44038ccfb829749c
AlteredCoder <AlteredCoder>
2021-01-07 18:07:20 +0100

    fix lua lib path

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ PLUGIN_PATH="/etc/nginx/lua/plugins/${PLUGIN_NAME}"
 
 mkdir -p ${PLUGIN_PATH}
 pwd
-git clone https://github.com/crowdsecurity/lua-cs-bouncer.git ${PLUGIN_PATH}/
+git clone https://github.com/crowdsecurity/lua-cs-bouncer.git
+cp lua-cs-bouncer/lib/*.lua /etc/nginx/lua/plugins/${PLUGIN_NAME}
 wget https://raw.githubusercontent.com/AlteredCoder/poc-ingress-nginx-bouncer/main/main.lua
 ls /etc/nginx/lua/plugins/
 cp main.lua ${PLUGIN_PATH}/
