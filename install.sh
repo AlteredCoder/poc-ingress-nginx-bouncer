23b9e25ffc6400bef1b6c90a25092eae069a0447
AlteredCoder <AlteredCoder>
2021-01-07 18:16:01 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 PLUGIN_NAME="cs-ingress-nginx-bouncer"
 PLUGIN_PATH="/etc/nginx/lua/plugins/${PLUGIN_NAME}"
-
+LIB_PATH="/etc/nginx/lua/"
 mkdir -p ${PLUGIN_PATH}
 git clone https://github.com/crowdsecurity/lua-cs-bouncer.git
-cp lua-cs-bouncer/lib/*.lua /etc/nginx/lua/plugins/${PLUGIN_NAME}
+cp lua-cs-bouncer/lib/*.lua ${LIB_PATH}
 wget https://raw.githubusercontent.com/AlteredCoder/poc-ingress-nginx-bouncer/main/main.lua
 ls /etc/nginx/lua/plugins/
 cp main.lua ${PLUGIN_PATH}/
